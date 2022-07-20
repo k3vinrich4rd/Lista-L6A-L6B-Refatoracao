cca3ecb140619343695f91afbe6449cfa8d34348
k3vinrich4rd <kevinrichardve@gmail.com>
2022-07-20 10:59:18 -0300

    Inserindo implementações feitas na classe Mãe

--- src/Funcionarios.java
+++ src/Funcionarios.java
@@ -1,3 +1,59 @@
 public abstract class Funcionarios {
+    private String nome;
+    private String cpf;
+    private String numeroDeRegistro;
+    private String orgaoDeLotacao;
+    private double salario;
 
+    public Funcionarios(String nome, String cpf, String numeroDeRegistro, String orgaoDeLotacao, double salario) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.numeroDeRegistro = numeroDeRegistro;
+        this.orgaoDeLotacao = orgaoDeLotacao;
+        this.salario = salario;
+    }
+
+    public int aumentarSalario() {
+        return (int) (salario = this.salario * 0.1);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNumeroDeRegistro() {
+        return numeroDeRegistro;
+    }
+
+    public void setNumeroDeRegistro(String numeroDeRegistro) {
+        this.numeroDeRegistro = numeroDeRegistro;
+    }
+
+    public String getOrgaoDeLotacao() {
+        return orgaoDeLotacao;
+    }
+
+    public void setOrgaoDeLotacao(String orgaoDeLotacao) {
+        this.orgaoDeLotacao = orgaoDeLotacao;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 }

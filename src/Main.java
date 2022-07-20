ad09ea64d7b915e9ce81d9f2c42f5ad27b23b02f
k3vinrich4rd <kevinrichardve@gmail.com>
2022-07-20 13:16:08 -0300

    Inserindo atualizações feitas na main, criação de objeto professor

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+        Professor professor1 = new Professor("Norberto", "333.333.333 - 33",
+                "333.333.333 - 33", "Secretária da educação - Sp", 1000,
+                "Mestre em química", "Química", 125, 5);
+        System.out.println(professor1.toString());
+
+
+    }
+
 }

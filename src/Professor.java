878a2deca8c63d15f2fdd54156f655c17b5dae79
k3vinrich4rd <kevinrichardve@gmail.com>
2022-07-21 20:29:27 -0300

    Inserindo atualizaçõs feitas na classe professor

--- src/Professor.java
+++ src/Professor.java
@@ -3,50 +3,80 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Professor extends Funcionarios {
-    private List<String> listaDeTurmas = new ArrayList<>();
+    private List<String> listaDeTurmas = new ArrayList<String>();
+    private List<String> listaDeEstagiarios = new ArrayList<String>();
     private String nivelDeGraduacao;
     private String disciplinaMinistrada;
     private int quantidadeDeAlunos;
     private int quantidadeDeTurmas;
+    private int quantidadeDeEstagiarios;
 
-
-    public Professor(String nome, String cpf, String numeroDeRegistro, String orgaoDeLotacao, double salario, String nivelDeGraduacao, String disciplinaMinistrada, int quantidadeDeAlunos, int quantidadeDeTurmas,
-                     List<String> listaDeTurmas) {
+    public Professor(String nome, String cpf, String numeroDeRegistro, String orgaoDeLotacao, double salario, String nivelDeGraduacao, String disciplinaMinistrada, int quantidadeDeAlunos, int quantidadeDeTurmas, int quantidadeDeEstagiarios) {
         super(nome, cpf, numeroDeRegistro, orgaoDeLotacao, salario);
         this.nivelDeGraduacao = nivelDeGraduacao;
         this.disciplinaMinistrada = disciplinaMinistrada;
         this.quantidadeDeAlunos = quantidadeDeAlunos;
-        this.quantidadeDeTurmas = quantidadeDeTurmas;
-        this.listaDeTurmas = listaDeTurmas;
-
 
     }
 
     @Override
-    public int aumentarSalario() {
-        return (int) (this.getSalario() * 0.1);
+    public String toString() {
+        System.out.println("--------------------------------");
+        System.out.println("Informações sobre o professor: ");
+        System.out.println("--------------------------------");
+        return "Nome do(a) professor(a): " + this.getNome() +
+                "\nCpf: " + this.getCpf() +
+                "\nNúmero de registro: " + this.getNumeroDeRegistro() +
+                "\nOrgão de lotação: " + this.getOrgaoDeLotacao() +
+                "\nSalário antes do aumento: R$ " + this.getSalario() +
+                "\nNível de graduação: " + this.getNivelDeGraduacao() +
+                "\nDisciplina Ministrada: " + this.getDisciplinaMinistrada() +
+                "\nQuantidade de alunos: " + this.getQuantidadeDeAlunos() +
+                "\nQuantidade de turmas: " + this.getQuantidadeDeTurmas() +
+                "\nTurmas adicionadas: " + getListaDeTurmas() +
+                "\nEstagiários supervisionados: " + getListaDeEstagiarios();
+
+
     }
 
-    public void adicionarTurma(String turmaAno) {
-        this.listaDeTurmas.add(turmaAno);
+
+    public void adicionarEstagiario(String estagiario) {
+        this.listaDeEstagiarios.add(estagiario);
+        this.setQuantidadeDeEstagiarios(this.getQuantidadeDeEstagiarios() + 1);
     }
 
+    public void adicionarTurma(String turmas) {
+        this.listaDeTurmas.add(turmas);
+        this.setQuantidadeDeTurmas(this.getQuantidadeDeTurmas() + 1);
+    }
+
+    @Override
+    public void reembolsoDespesas() {
+
+    }
+
+    @Override
+    public void aumentoDeSalario() {
+        System.out.println();
+        double aumento = this.getSalario() * 0.1;
+        System.out.println("O aumento feito em seu salário foi de 10% : R$ " + aumento);
+
+    }
 
     public List<String> getListaDeTurmas() {
         return listaDeTurmas;
     }
 
-    private String getInformacoesListasTurmas() {
-        String informacoesListasTurma = ",";
-        for (String info : listaDeTurmas) {
-            informacoesListasTurma = "," + info;
-        }
-        return informacoesListasTurma;
-    }
-
-
     public void setListaDeTurmas(List<String> listaDeTurmas) {
         this.listaDeTurmas = listaDeTurmas;
+    }
+
+    public List<String> getListaDeEstagiarios() {
+        return listaDeEstagiarios;
+    }
+
+    public void setListaDeEstagiarios(List<String> listaDeEstagiarios) {
+        this.listaDeEstagiarios = listaDeEstagiarios;
     }
 
     public String getNivelDeGraduacao() {
@@ -81,22 +111,14 @@ public class Professor extends Funcionarios {
         this.quantidadeDeTurmas = quantidadeDeTurmas;
     }
 
-
-    @Override
-    public String toString() {
-        System.out.println("------------------------------");
-        System.out.println("Informações sobre o professor: ");
-        System.out.println("------------------------------");
-        return "Nome do professor: " + this.getNome() +
-                "\nCpf: " + this.getCpf() + "\nNúmero de registro " + this.getNumeroDeRegistro() +
-                "\nOrgão de lotação: " + this.getOrgaoDeLotacao() + "\nSalário: " + this.getSalario() +
-                "\nNível de graduação do professor: " + this.getNivelDeGraduacao() +
-                "\nDisciplina Ministrada: " + this.getDisciplinaMinistrada() +
-                "\nQuantidade de alunos: " + this.getQuantidadeDeAlunos() +
-                "\nQuantidade de turmas: " + this.getQuantidadeDeTurmas() +
-                "\nAumento de salário no valor de 10%: R$" + this.aumentarSalario() +
-                "\nTurmas adicionadas: " + getListaDeTurmas();
-
-
+    public int getQuantidadeDeEstagiarios() {
+        return quantidadeDeEstagiarios;
     }
+
+    public void setQuantidadeDeEstagiarios(int quantidadeDeEstagiarios) {
+        this.quantidadeDeEstagiarios = quantidadeDeEstagiarios;
+    }
+
+
 }
+

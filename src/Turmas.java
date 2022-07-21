e909858d9b85295c1b56a0a81850e4018351b6c3
k3vinrich4rd <kevinrichardve@gmail.com>
2022-07-21 20:30:24 -0300

    Inserindo atualizações de criação, classe turmas

--- src/Turmas.java
+++ src/Turmas.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Turmas {
+public class Turmas {
+    private String nome;
+
+    public Turmas(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
